2bcc877dc979b9a132832036b6f2a045293db65b
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-27 13:10:31 +0100

    Created models for store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,137 @@
+""" This file contains the models for the store app. """
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class ProductCatgeory(models.Model):
+    """
+    This class represents a product category in the store.
+    
+    Attributes:
+        id (int): The primary key for the product category.
+        title (str): The title of the product category.
+        created_at (datetime): The date and time the product category was created.
+        updated_at (datetime): The date and time the product category was last updated.
+    """
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    """
+    This class represents a product in the store.
+
+    Attributes:
+        id (int): The primary key for the product.
+        created_at (datetime): The date and time the product was created.
+        updated_at (datetime): The date and time the product was last updated.
+        title (str): The title of the product.
+        description (str): The description of the product.
+        price (decimal): The price of the product.
+        inventory (int): The number of the product in inventory.
+        collection (ProductCatgeory): The product category the product belongs to.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    collection = models.ForeignKey(ProductCatgeory, on_delete=models.PROTECT)
+
+
+class User(models.Model):
+    """
+    This class represents a user in the store.
+
+    Attributes:
+        id (int): The primary key for the user.
+        created_at (datetime): The date and time the user was created.
+        updated_at (datetime): The date and time the user was last updated.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        email (str): The email of the user.
+        phone (str): The phone number of the user.
+        password (str): The password of the user.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    password = models.CharField(max_length=20)
+
+class Order(models.Model):
+    """
+    This class represents an order in the store.
+    
+    Attributes:
+        id (int): The primary key for the order.
+        placed_at (datetime): The date and time the order was created.
+        user (User): The user who placed the order.
+
+        PAYMENT_STATUS_PENDING (str): The order is pending payment.
+        PAYMENT_STATUS_COMPLETE (str): The order has been paid for.
+        PAYMENT_STATUS_FAILED (str): The order payment failed.
+        PAYMENT_STATUS_CHOICES (list): The list of payment status choices.
+        payment_status (str): The payment status of the order.
+    """
+    placed_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    # This is an enum for the payment status of the order.
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
+class OrderItem(models.Model):
+    """
+    This class represents an order item in the store.
+
+    Attributes:
+        id (int): The primary key for the order item.
+        order (Order): The order the order item belongs to.
+        product (Product): The product the order item is for.
+        quantity (int): The quantity of the product ordered.
+        unit_price (decimal): The unit price of the product ordered.
+    """
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    """
+    This class represents a cart in the store.
+
+    Attributes:
+        id (int): The primary key for the cart.
+        created_at (datetime): The date and time the cart was created.
+        updated_at (datetime): The date and time the cart was last updated.
+        user (User): The user who owns the cart.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    """
+    This class represents a cart item in the store.
+
+    Attributes:
+        id (int): The primary key for the cart item.
+        cart (Cart): The cart the cart item belongs to.
+        product (Product): The product the cart item is for.
+        quantity (int): The quantity of the product ordered.
+        unit_price (decimal): The unit price of the product ordered.
+    """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
