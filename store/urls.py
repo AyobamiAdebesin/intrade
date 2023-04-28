54ddfca8881c394797278a3a350703d8478ce39a
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-28 15:27:58 +0100

    Added new endpoints

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('users/', views.user_list, name='user_list'),
     path('users/<int:pk>/', views.user_detail, name='user_detail'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:pk>', views.product_detail, name='product_detail')
 ]
