dac73c0d28a17e59a53807994f87b4fc162b60e5
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-28 09:19:07 +0100

    Defined view functions for api

--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
 
 # Create your views here.
+
+@api_view(['GET'])
+def user_list(request):
+    """
+    This function handles the request to list all users.
+    """
+    return Response('Hello, world. You\'re at the users index.')
+
+@api_view()
+def user_detail(request, pk):
+    """
+    This function handles the request to retrieve a user.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=404)
+    return Response(f'Hello, world. You\'re at the user {user.first_name} detail.')
