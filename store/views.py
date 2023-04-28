29b4ad70b280414007382cc519773e9a31fdf662
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-28 15:28:30 +0100

    Added controllers for endpoints

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import User
+from .models import Product
+from .serializers import UserSerializer, ProductSerializer
 
 # Create your views here.
 
@@ -11,15 +14,42 @@ def user_list(request):
     """
     This function handles the request to list all users.
     """
-    return Response('Hello, world. You\'re at the users index.')
+    users = User.objects.all()[:5]
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def user_detail(request, pk):
     """
     This function handles the request to retrieve a user.
     """
+    user = get_object_or_404(User, pk=pk)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_list(request):
     try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=404)
-    return Response(f'Hello, world. You\'re at the user {user.first_name} detail.')
+        products = Product.objects.select_related('category').all()
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+# @api_view(['GET'])
+# def get_products_by_category(request, pk):
+#     """
+#     Get all the products in the producst category
+#     table by category_id = pk
+#     """
+
+#     products = Product.objects.select_related('category').filter(category_id=pk)
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)
