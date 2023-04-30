4f3656ae5286a560b14f105492d61d413283a0de
AyobamiAdebesin <froshofficial@gmail.com>
2023-05-01 00:44:30 +0100

    Converted function views to class views

--- store/urls.py
+++ store/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list, name='user_list'),
-    path('users/<int:pk>/', views.user_detail, name='user_detail'),
-    path('products/', views.product_list, name='product_list'),
-    path('products/<int:pk>', views.product_detail, name='product_detail'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path('products/', views.ProductList.as_view(), name='product_list'),
+    path('products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
     path('categories/', views.category_list, name='category_list'),
     path('categories/<int:pk>', views.category_detail, name='category_detail'),
-]
+] 
