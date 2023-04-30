c35ece77d8bb91ec0a81dabe7914b3ba066b4c2d
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-30 23:17:43 +0100

    Added categories endpoint to urls path

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('users/', views.user_list, name='user_list'),
     path('users/<int:pk>/', views.user_detail, name='user_detail'),
     path('products/', views.product_list, name='product_list'),
-    path('products/<int:pk>', views.product_detail, name='product_detail')
+    path('products/<int:pk>', views.product_detail, name='product_detail'),
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/<int:pk>', views.category_detail, name='category_detail'),
 ]
