8ff6758e92c1c9308fcd4d6bece469ca75106568
AyobamiAdebesin <froshofficial@gmail.com>
2023-05-02 13:00:16 +0100

    Added url endpoint for cart creation

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
     path('products/', views.ProductList.as_view(), name='product_list'),
     path('products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('carts/', views.CartCreation.as_view(), name='cart_list'),
+    path('carts/<pk>', views.CartDetail.as_view(), name='cart_detail'),
     path('categories/', views.category_list, name='category_list'),
     path('categories/<int:pk>', views.category_detail, name='category_detail'),
 ] 
