6366ed0f96f75a16be870a4e1c7145f4c31608f3
AyobamiAdebesin <froshofficial@gmail.com>
2023-05-02 14:40:44 +0100

    Rounding up project: Completed the views classes

--- store/views.py
+++ store/views.py
@@ -1,135 +1,170 @@
+from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
+from store.pagination import DefaultPagination
+from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-from django.db.models import Count
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action, permission_classes
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
-from .models import User, Cart
-from .models import Product, ProductCatgeory
-from .serializers import UserSerializer, ProductSerializer, CartSerializer
-from .serializers import ProductCategorySerializer
-
-# Create your views here.
+from .filters import ProductFilter
+from .models import Cart, CartItem, Collection, Customer, Order, OrderItem, Product, Review
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CollectionSerializer, CreateOrderSerializer, CustomerSerializer, OrderSerializer, ProductSerializer, ReviewSerializer, UpdateCartItemSerializer, UpdateOrderSerializer
 
 
-class ProductList(APIView):
+class ProductViewSet(ModelViewSet):
     """
-    This class handles the request to list all products.
+    This class defines the create, retrieve, update, and destroy actions
+    for the Product model.
     """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    permission_classes = [IsAdminOrReadOnly]
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
-    def get(self, request):
-        products = Product.objects.select_related('category').all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+    def get_serializer_context(self):
+        return {'request': self.request}
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
+
+
+class CollectionViewSet(ModelViewSet):
+    """
+    This class defines the create, retrieve, update, and destroy actions
+    for the Collection model.
+    """
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']):
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
+
+
+class ReviewViewSet(ModelViewSet):
+    """
+    This class defines the create, retrieve, update, and destroy actions
+    for the Review model.
+    """
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+
+
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
+    """ This class defines the create, retrieve, and destroy actions
+    for the Cart model.
+    """
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    """
+    This class defines the create, retrieve, update, and destroy actions
+    for the CartItem model.
+    """
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+
+    def get_queryset(self):
+        return CartItem.objects \
+            .filter(cart_id=self.kwargs['cart_pk']) \
+            .select_related('product')
+
+
+class CustomerViewSet(ModelViewSet):
+    """
+    This class defines the create, retrieve, update, and destroy actions
+    for the Customer model.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=True, permission_classes=[ViewCustomerHistoryPermission])
+    def history(self, request, pk):
+        return Response('ok')
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        customer = Customer.objects.get(
+            user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
+
+class OrderViewSet(ModelViewSet):
+    """
+    This class defines the create, retrieve, update, and destroy actions
+    for the Order model.
+    """
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id})
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class UserList(APIView):
-    """
-    This class handles the request to list all users.
-    """
-
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
         return Response(serializer.data)
 
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
 
-class UserDetail(APIView):
-    """
-    This class handles the request to retrieve a user.
-    """
+    def get_queryset(self):
+        user = self.request.user
 
-    def get(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+        if user.is_staff:
+            return Order.objects.all()
 
-
-class ProductDetail(APIView):
-    """
-    This class handles the request to retrieve a product.
-    """
-
-    def get(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.order_items.count() > 0:
-            return Response({'error': 'Product cannot be deleted'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CartCreation(APIView):
-    """ This class handles the request to create carts. """
-
-    def post(self, request):
-        serializer = CartSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class CartDetail(APIView):
-    """ This class handles the request to retrieve a cart and its items. """
-
-    def get(self, request, pk):
-        cart = get_object_or_404(Cart, pk=pk)
-        serializer = CartSerializer(cart)
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        cart = get_object_or_404(Cart, pk=pk)
-        cart.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def category_list(request):
-    """ Fetch all categories from the database """
-    if request.method == 'GET':
-        categories = ProductCatgeory.objects.annotate(
-            product_count=Count('products')).all()
-        serializer = ProductCategorySerializer(categories, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductCategorySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['GET', 'PATCH', 'DELETE'])
-def category_detail(request, pk):
-    category = get_object_or_404(ProductCatgeory.objects.annotate(
-        products_count=Count('products')), pk=pk)
-    if request.method == 'GET':
-        serializer = ProductCategorySerializer(category)
-        return Response(serializer.data)
-    elif request.method == 'PATCH':
-        serializer = ProductCategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-    elif request.method == 'DELETE':
-        if category.products.count() > 0:
-            return Response({'error': 'Category cannot be deleted.'})
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        customer_id = Customer.objects.only(
+            'id').get(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
